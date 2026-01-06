c1d44eeda6c486af140c6f26c541d91116936ffe
廿一 <herojsme@163.com>
2026-01-06 17:42:56 +0800

    提交修改代码

---
+++ myLoader.js
@@ -0,0 +1,6 @@
+export default function myImageLoader({ src, width, quality }) {
+//   return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+
+  return src //本地
+  // return "/staticBlob"+src //pages
+}
---
+++ next.config.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+
+  assetPrefix: './',
+  // basePath: '/staticBlob',
+  // assetPrefix: '/staticBlob/',
+  images: {
+    loader: 'custom',
+    loaderFile: './myLoader.js',
+  },
+}
+
+module.exports = nextConfig
